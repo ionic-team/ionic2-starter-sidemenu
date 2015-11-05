d09a367359159b8c44c9fbc9b2d184f0730c6066
Tim Lancina <tim@ionic.io>
2015-11-04 22:47:07 -0600

    initial commit

---
+++ www/app/getting-started/getting-started.js
@@ -0,0 +1,12 @@
+import {Page, NavController} from 'ionic/ionic';
+
+
+@Page({
+  templateUrl: 'app/getting-started/getting-started.html'
+})
+export class GettingStartedPage {
+  constructor(nav: NavController) {
+
+  }
+
+}
---
+++ www/app/grid/grid.js
@@ -0,0 +1,7 @@
+import {Page} from 'ionic/ionic';
+
+
+@Page({
+  templateUrl: 'app/grid/grid.html'
+})
+export class GridPage {}
