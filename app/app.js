62b6bc0b78277af593186ba503c661440f534821
Tim Lancina <tim@ionic.io>
2016-02-19 14:48:01 -0600

    chore(): remove ionic alias

--- app/app.js
+++ app/app.js
@@ -1,4 +1,4 @@
-import {App, IonicApp, Platform} from 'ionic/ionic';
+import {App, IonicApp, Platform} from 'ionic-framework/ionic';
 import {Inject} from 'angular2/core';
 import {GettingStartedPage} from './pages/getting-started/getting-started';
 import {ListPage} from './pages/list/list';
