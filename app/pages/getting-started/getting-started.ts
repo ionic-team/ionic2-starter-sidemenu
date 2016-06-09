5dfdd093bdfa64b004c808eadefc305230ac6f28
Dan Bucholtz <dan.bucholtz@gmail.com>
2016-06-09 11:44:10 -0500

    update ts starter

--- app/pages/getting-started/getting-started.ts
+++ app/pages/getting-started/getting-started.ts
@@ -1,11 +1,16 @@
-import {Component} from "@angular/core";
-
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
+import {ScientificFactsPage} from '../scientific-facts-page/scientific-facts-page';
 
 @Component({
   templateUrl: 'build/pages/getting-started/getting-started.html'
 })
 export class GettingStartedPage {
-  constructor() {
 
+  constructor(private _navController:NavController) {
+  }
+
+  goToFactsPage(){
+    this._navController.push(ScientificFactsPage);
   }
 }
