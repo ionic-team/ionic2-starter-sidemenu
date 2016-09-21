07aa01ade6101a531b9e54d4206dc542482a6809
Brandy Carney <brandyscarney@gmail.com>
2016-09-20 22:41:39 -0400

    refactor(app): update structure for beta 12

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,21 @@
-import { Component, ViewChild } from '@angular/core';
-import { ionicBootstrap, Platform, Nav } from 'ionic-angular';
-import { StatusBar } from 'ionic-native';
+import { Component } from '@angular/core';
+import { Nav, Platform } from 'ionic-angular';
+// import { StatusBar } from 'ionic-native';
 
-import { Page1 } from './pages/page1/page1';
-import { Page2 } from './pages/page2/page2';
+import { Page1 } from '../pages/page1/page1';
+
 
 @Component({
-  templateUrl: 'build/app.html'
+  template: `<ion-nav [root]="rootPage"></ion-nav>`
 })
-class MyApp {
+export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
   rootPage: any = Page1;
 
   pages: Array<{title: string, component: any}>;
 
-  constructor(private platform: Platform) {
+  constructor(public platform: Platform) {
     this.initializeApp();
 
     // used for an example of ngFor and navigation
@@ -30,7 +30,7 @@ class MyApp {
     this.platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      StatusBar.styleDefault();
+      // StatusBar.styleDefault();
     });
   }
 
@@ -40,5 +40,3 @@ class MyApp {
     this.nav.setRoot(page.component);
   }
 }
-
-ionicBootstrap(MyApp);
