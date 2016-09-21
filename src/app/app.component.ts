ff80ba486d5712ca104a04145df99abeda4d20c5
Brandy Carney <brandyscarney@gmail.com>
2016-09-20 23:00:07 -0400

    refactor(app): add menu and imports and fix buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { Nav, Platform } from 'ionic-angular';
 // import { StatusBar } from 'ionic-native';
 
 import { Page1 } from '../pages/page1/page1';
+import { Page2 } from '../pages/page2/page2';
 
 
 @Component({
-  template: `<ion-nav [root]="rootPage"></ion-nav>`
+  templateUrl: 'app.html'
 })
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
@@ -20,8 +21,8 @@ export class MyApp {
 
     // used for an example of ngFor and navigation
     this.pages = [
-      { title: 'Page uno', component: Page1 },
-      { title: 'Page dos', component: Page2 }
+      { title: 'Page One', component: Page1 },
+      { title: 'Page Two', component: Page2 }
     ];
 
   }
