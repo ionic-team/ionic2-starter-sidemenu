2c037d4f7bad20c3daa0a19da104723ae0f8ff31
Justin Willis <jgw9617@gmail.com>
2016-09-26 15:15:09 -0500

    chore(): uncomment ionic-native

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, ViewChild } from '@angular/core';
 import { Nav, Platform } from 'ionic-angular';
-// import { StatusBar } from 'ionic-native';
+import { StatusBar } from 'ionic-native';
 
 import { Page1 } from '../pages/page1/page1';
 import { Page2 } from '../pages/page2/page2';
@@ -31,7 +31,7 @@ export class MyApp {
     this.platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      // StatusBar.styleDefault();
+      StatusBar.styleDefault();
     });
   }
 
